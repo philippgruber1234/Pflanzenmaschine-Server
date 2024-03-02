32fe87b4041fb89851f66b8cf0998297f04e514f
philippgruber1234 <philipp_thomas.gruber@gmx.de>
2024-03-02 12:34:48 +0100

    Neuer Code
    Ich habe eine API für die API erstellt.

--- Pflanzenmaschine_Server.py
+++ Pflanzenmaschine_Server.py
@@ -1,1 +1,21 @@
-
+print("PFLANZENMASCHINE-SERVER")
+print("Import modules")
+from flask import *
+print("ALERT <INIT> ALERT")
+ids=list()
+keys=list()
+strs=list()
+print("Starting up...")
+app = Flask(__name__)
+def createkey(id, key, str):
+    ids.append(id)
+    keys.append(key)
+    strs.append(str)
+    return id
+def removekey(id):
+    rmid=ids.index(id)
+    ids.remove(rmid)
+    keys.remove(rmid)
+    strs.remove(rmid)
+    return rmid
+def getkey(id):
